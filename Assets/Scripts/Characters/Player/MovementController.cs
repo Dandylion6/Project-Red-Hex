a81eef753f43cb3ed4c64bd523ba39d0c6b683da
Dandy <daanosterloh.06@gmail.com>
2026-06-22 14:36:56 +0200

    Can select and de-select player to move

--- Assets/Scripts/Characters/Player/MovementController.cs
+++ Assets/Scripts/Characters/Player/MovementController.cs
@@ -7,6 +7,7 @@ public class MovementController : MonoBehaviour
     private TilePiece playerPiece = null;
     private Camera mainCamera = null;
     private Vector2 currentScreenPosition = Vector2.zero;
+    private bool playerSelected = false;
 
 
     public void OnScreenInput(InputAction.CallbackContext context) => currentScreenPosition = context.ReadValue<Vector2>();
@@ -15,13 +16,27 @@ public class MovementController : MonoBehaviour
     public void OnInputClick(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
+        if (!GetHitTile(out HexTile tile)) return;
 
+        if (tile.Piece == GameManager.Instance.Player)
+        {
+            playerSelected = !playerSelected;
+            return;
+        }
+
+        if (playerSelected)
+            playerPiece.MoveTo(tile);
+    }
+
+
+    private bool GetHitTile(out HexTile tile)
+    {
+        tile = null;
         Ray ray = mainCamera.ScreenPointToRay(currentScreenPosition);
 
-        if (!Physics.Raycast(ray, out RaycastHit hit)) return;
-        if (!hit.collider.TryGetComponent(out HexTile tile)) return;
-
-        playerPiece.MoveTo(tile);
+        if (!Physics.Raycast(ray, out RaycastHit hit)) return false;
+        if (!hit.collider.TryGetComponent(out tile)) return false;
+        return true;
     }
 
 
