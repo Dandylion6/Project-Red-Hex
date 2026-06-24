6636b91705a1b28b422eee4b27f2535045784d2e
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2026-06-24 15:14:39 +0200

    combat evolved

--- Assets/Scripts/Hotbar/Item.cs
+++ Assets/Scripts/Hotbar/Item.cs
@@ -1,12 +1,24 @@
+using Unity.Hierarchy;
 using UnityEngine;
 [System.Serializable]
 public abstract class Item : MonoBehaviour
 {
-    public TilePiece Player => player;
 
-    private TilePiece player = null;
 
-        
+    private TilePiece player;
+
+    private TileSelect tileSelect;
+
+    
+   
+    protected TilePiece Player => player;
+    protected TileSelect TileSelect => tileSelect;
+
+    public void Start()
+    {
+        player = GameManager.Instance.Player;
+        tileSelect = player.GetComponent<TileSelect>();
+    }
     public abstract void Use();
 
 
