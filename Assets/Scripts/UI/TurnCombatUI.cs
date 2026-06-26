ad743198a3c6d783c43afa5f7398138cf31e7410
Dandy <daanosterloh.06@gmail.com>
2026-06-26 21:14:42 +0200

    Item description working

--- Assets/Scripts/UI/TurnCombatUI.cs
+++ Assets/Scripts/UI/TurnCombatUI.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,6 +10,10 @@ public class TurnCombatUI : MonoBehaviour
     [SerializeField] private HotBarSlotUI prefabSlot = null;
     [SerializeField] private RectTransform slotParent = null;
     [SerializeField] private TMP_Text turnCounter = null;
+    [SerializeField] private CanvasGroup descriptionBox = null;
+    [SerializeField] private TMP_Text itemNameText = null;
+    [SerializeField] private TMP_Text descriptionText = null;
+    [SerializeField] private TMP_Text statsText = null;
 
 
     private HotBar hotBar = null;
@@ -22,15 +28,50 @@ public class TurnCombatUI : MonoBehaviour
             HotBarSlotUI slotUI = Instantiate(prefabSlot, slotParent);
             slotUI.Initialize(hotBar, item);
         }
+
+        TurnManager.Instance.SubscribeToOnTurnChanged(OnTurnChanged);
+        HotBar.Instance.SubscribeToOnSelectionChanged(OnSelectionChanged);
+
+        canvas.enabled = false;
+        descriptionBox.alpha = 0.0f;
     }
 
 
-    private void Update()
+    private void OnTurnChanged(TilePiece currentPiece, TilePiece lastPiece)
     {
         bool stateChanged = TurnManager.Instance.IsInCombat != canvas.enabled;
         if (stateChanged)
             canvas.enabled = TurnManager.Instance.IsInCombat;
 
         turnCounter.text = "~ Turn " + TurnManager.Instance.TurnsInCombat + " ~";
+    }
+
+
+    private void OnSelectionChanged(Item currentItem, Item lastItem)
+    {
+        if (currentItem == null)
+        {
+            descriptionBox.alpha = 0.0f;
+            return;
+        }
+
+        descriptionBox.alpha = 1.0f;
+        itemNameText.text = currentItem.BaseData.DisplayName;
+        descriptionText.text = currentItem.BaseData.Description;
+
+        statsText.text = string.Empty;
+        Queue<ItemStatEntry> entries = currentItem.BaseData.GetStats();
+        while (entries.Count > 0)
+        {
+            ItemStatEntry entry = entries.Dequeue();
+            statsText.text += entry.label + " - " + entry.value + "\n";
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        TurnManager.Instance.UnsubscribeFromOnTurnChanged(OnTurnChanged);
+        HotBar.Instance.UnsubscribeFromOnSelectionChanged(OnSelectionChanged);
     }
 }
