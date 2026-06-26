aa77a7a5ebe0ea8926ebadb251f76219e94d6f46
Dandy <daanosterloh.06@gmail.com>
2026-06-26 15:22:26 +0200

    Teleport change

--- Assets/Scripts/Environment/SpawnPoint.cs
+++ Assets/Scripts/Environment/SpawnPoint.cs
@@ -1,44 +1,14 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(HexTile))]
 public class SpawnPoint : MonoBehaviour
 {
-    static private string lastScene = string.Empty;
-
-    [Header("Spawn Settings")]
-    [SerializeField] private string currentScene = "Current Scene";
-    [SerializeField] private string goToScene = "Next Scene";
-
-    private HexTile tile = null;
-
-
     private void Start()
     {
-        tile = GetComponent<HexTile>();
+        HexTile tile = GetComponent<HexTile>();
         TilePiece player = GameManager.Instance.Player;
 
-        // Will move the player to this tile.
-        if (lastScene == goToScene)
-        {
-            player.SpawnAt(tile);
-            PlayerCamera.Instance.SnapToTarget(tile);
-        }
-
-        tile.SubscribeToOnPiecePlaced(MoveToNextScene);
+        player.SpawnAt(tile);
+        PlayerCamera.Instance.SnapToTarget(tile);
     }
-
-
-    private void MoveToNextScene(TilePiece piece)
-    {
-        // Only check for player
-        if (piece != GameManager.Instance.Player) return;
-
-        if (currentScene != string.Empty) SceneManager.UnloadSceneAsync(currentScene);
-        lastScene = currentScene;
-        if (goToScene != string.Empty) SceneManager.LoadScene(goToScene, LoadSceneMode.Additive);
-    }
-
-
-    private void OnDestroy() => tile.UnsubscibeToOnPiecePlaced(MoveToNextScene);
 }
