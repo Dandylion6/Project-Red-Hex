48aadd38655014616be2876fb58b197819003a32
Georgi K. <georgikostov333@gmail.com>
2026-07-01 13:16:40 +0200

    Pathfinding

--- Assets/Scripts/Utility/Pathfinding.cs
+++ Assets/Scripts/Utility/Pathfinding.cs
@@ -74,7 +74,6 @@ public class Pathfinding
 
                 if (!neighbor.IsWalkable) continue;
                 if (neighbor.IsObstacle) continue;
-                if (neighbor.Piece != null) continue;
                 if (closedTiles.Contains(neighbor)) continue;
 
                 int tentativeG = nodes[current].g + 1;
