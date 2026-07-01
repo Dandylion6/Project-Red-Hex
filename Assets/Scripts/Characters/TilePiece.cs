105a68fc7db580c4507076d0c97532eb3b947d98
Dandy <daanosterloh.06@gmail.com>
2026-07-01 16:47:05 +0200

    Pathfinding fix

--- Assets/Scripts/Characters/TilePiece.cs
+++ Assets/Scripts/Characters/TilePiece.cs
@@ -110,7 +110,8 @@ public class TilePiece : MonoBehaviour, IDamageable
     public bool MoveTo(HexTile tile)
     {
         if (!tile.CanSetPiece(this)) return false;
-        if (occupying != null) 
+
+        if (occupying != null)
             occupying.RemovePiece();
 
         occupying = tile;
@@ -124,7 +125,6 @@ public class TilePiece : MonoBehaviour, IDamageable
                 .OnComplete(() =>
                 {
                     tile.SetPiece(this);
-                    occupying = tile;
                     TurnManager.Instance.EndTurn();
                     onMove?.Invoke(tile);
                 }).Play();
