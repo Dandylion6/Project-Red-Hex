79977facb262e5f6bbe67dcce7bfdd3c427586a3
Dandy <daanosterloh.06@gmail.com>
2026-07-01 11:47:05 +0200

    Combo changed

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -53,14 +53,4 @@ public class Enemy : TilePiece
         if (TurnManager.Instance == null) return;
         TurnManager.Instance.UnsubscribeFromOnTurnChanged(OnTurnChanged);
     }
-
-
-    protected override void OnDestroy()
-    {
-        base.OnDestroy();
-        if (IsDead) return;
-
-        if (TurnManager.Instance == null) return;
-        TurnManager.Instance.UnsubscribeFromOnTurnChanged(OnTurnChanged);
-    }
 }
